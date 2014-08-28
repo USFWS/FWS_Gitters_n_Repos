3d6439314390c926ea20bc0256fa6d2379fb4ab6
Daryl Van Dyke <daryl_van_dyke@fws.gov>
2014-08-28 16:23:58 -0700

    version 2 of the script
    Here it is folks:  we need to-
    --->   configure the output to create the listing on GitHub
    --->   decide on a flag for 'not FWS' to manage the flow

---
+++ FWS_GitHub_inventory_v2.py
@@ -0,0 +1,69 @@
+import urllib2
+
+github = "https://github.com"
+githubPeeps = "https://github.com/orgs/USFWS/people"
+gitRepoSuffix = "?tab=repositories"
+
+tagIn = ' <a class="member-link" href=' 
+tagOut = '" itemprop="url">'
+
+tagIn2  =  '<h3 class="repo-list-name">'
+tagOut2 =  '">'
+
+
+listGitters = []
+listRepos = []
+
+def AmIForked():
+    closeTag = html.find("</h3>")
+    closeCloseTag = html[closeTag:].find("<h3 class")
+    #print html[closeTag:closeCloseTag]
+    if "forked from" in html[closeTag:closeCloseTag]: return True
+    else:  return False
+
+response = urllib2.urlopen(githubPeeps)
+html = response.read()
+
+start = html.find(tagIn)
+#print start
+
+if tagIn in html:
+    start = html.find(tagIn)
+    end = html.find(tagOut)
+
+    readStart = start+len(tagIn) + 2
+
+    print html[readStart:end]
+    listGitters.append( html[readStart:end] )
+
+    html = html[readStart:]
+
+for items in listGitters:
+    gitterHub = github +"/"+ items + gitRepoSuffix
+    #print gitterHub
+    response = urllib2.urlopen(gitterHub)
+    html = response.read()
+    #print html
+    logical = True
+    while logical:
+        start = html.find(tagIn2)
+        readStart = start+len(tagIn2) + 14
+        html = html[readStart:]
+        end = html.find(tagOut2)
+
+        strRepo = github+html[:end]
+        print strRepo
+        testRepo = AmIForked()
+        print "forked:",testRepo
+        if not testRepo: listRepos.append([items,strRepo])
+
+        # Test for the last element on the page
+        if tagIn2 in html:
+            print "TRUE"
+        else:
+            logical = False
+        #print html
+
+for rows in listRepos:
+    print listRepos
+    
