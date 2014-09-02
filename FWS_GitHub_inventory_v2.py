e5134293d3e7fa67045564d68814991f8ded5223
Daryl Van Dyke <daryl_van_dyke@fws.gov>
2014-09-02 15:28:00 -0700

    Added HTML generator
    I got a business doing web sites
    When my friends need some code
    who do they call?
    I do HTML for them all
    Even made a homepage for my dog!
    - W. A. Yankovich

--- FWS_GitHub_inventory_v2.py
+++ FWS_GitHub_inventory_v2.py
@@ -1,5 +1,8 @@
-import urllib2
+import urllib2,os
 
+###################
+#  Constants and Declares
+###################
 github = "https://github.com"
 githubPeeps = "https://github.com/orgs/USFWS/people"
 gitRepoSuffix = "?tab=repositories"
@@ -13,6 +16,9 @@ tagOut2 =  '">'
 listGitters = []
 listRepos = []
 
+#################
+#  local subs
+#################
 def AmIForked():
     closeTag = html.find("</h3>")
     closeCloseTag = html[closeTag:].find("<h3 class")
@@ -20,6 +26,37 @@ def AmIForked():
     if "forked from" in html[closeTag:closeCloseTag]: return True
     else:  return False
 
+def SimpleTwoColumnTable( inputList , outputFile = "GIT_users.htm"):
+    '''  A supersimple 2-column HTML Page Generator
+	         inputList is a list-of-lists n-length of ['user', 'url_path_to_repo']
+	'''
+    def writeRow(inputRow,f):
+        f.write( "<tr> \n" )
+        for items in inputRow:
+            outString = '''<a href="'''+items[1]+'''" target="_blank">'''+ os.path.split(items[0])[1]+'''</a> '''
+            f.write( "  <td>" +outString + "</td> \n" )
+        f.write( "</tr> \n" )
+    
+    line1 = "	<!DOCTYPE html> \n \n <html> \n \n <head> \n <style> \n"
+    line2 =  "table, th, td { border: 1px solid black; \n \n </style> \n \n </head> \n \n } "
+    line3 = ''' <body> \n \n <table style="width:100%"> \n '''
+    line4 = " </table> \n </body> \n </html> "
+	
+    f = open(outputFile, "w")
+    f.write(line1)
+    f.write(line2)
+    f.write(line3)
+    
+    for pairs in inputList:
+        writeRow(pairs,f)
+    
+    f.write(line4)
+    f.close()
+	
+
+#######
+#  Programs Starts Here
+#######
 response = urllib2.urlopen(githubPeeps)
 html = response.read()
 
@@ -55,7 +92,7 @@ for items in listGitters:
         testRepo = AmIForked()
         print "forked:",testRepo
         
-        if not testRepo: listRepos.append([items,strRepo])
+        if not testRepo: listRepos.append([(items,gitterHub),(strRepo,strRepo)])
 
         # Test for the last element on the page
         if tagIn2 in html:
@@ -65,5 +102,6 @@ for items in listGitters:
         #print html
 ######  BOGUS OUTPUT #####  create --> output to Sharepoint
 for rows in listRepos:
-    print rows
-    
+    print rows[0][0],"--->", rows[1][1]
+######    REAL OUTPUT #####   Defaults to "GitHub.htm" in same directory	
+SimpleTwoColumnTable(listRepos)
